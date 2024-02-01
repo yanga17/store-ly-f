2ac5542fbcb74400e79acf4bf5c6accc132a3459
BranbdonOnlin3 <Brandonnkawu01@gmail.com>
2024-02-01 16:39:08 +0200

    adds product view, with edit, view and page links

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Page({ params }: { params: { emp_id: string } }) {
+    const { emp_id } = params
+
+    return (
+        <div>Edit product here</div>
+    )
+}
--- src/modules/products/view.tsx
+++ src/modules/products/view.tsx
@@ -2,10 +2,10 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
-import { colors } from '@/utils/colors';
-import { ArrowDown, Utensils } from 'lucide-react';
-import { useQuery } from '@/hooks/useQuery';
 import { Products } from '@/hooks';
+import { colors } from '@/utils/colors';
+import { ArrowDown, Eye, EyeOff, Pencil, Utensils } from 'lucide-react';
+import Link from 'next/link';
 
 interface PageProps {
     operator: string;
@@ -31,13 +31,7 @@ export const ViewProductsModule = () => {
 
     const toggleAccordion = (ref: number) => setIsExpanded(ref)
 
-
     const products = Products()
-
-    console.log(products)
-
-    const url = `api/v1/machine/getproductionview`
-    const { data, loading, error } = useQuery<ExpectedData>(url)
 
     const loadingUI = new Array(30).fill(null);
 
@@ -45,7 +39,7 @@ export const ViewProductsModule = () => {
         return (
             <>
                 {loadingUI?.map((index) =>
-                    <div className="flex items-center justify-between bg-white p-2 rounded" key={index}>
+                    <div className="flex items-center justify-between bg-white p-2 rounded divide-x divide-greyMid" key={index}>
                         <div className="rounded bg-grey-darker animate-pulse py-3 w-[23%] mx-auto" />
                         <div className="rounded bg-grey-darker animate-pulse py-8 w-[23%] mx-auto" />
                         <div className="rounded bg-grey-darker animate-pulse py-3 w-[23%] mx-auto" />
@@ -56,34 +50,13 @@ export const ViewProductsModule = () => {
         )
     }
 
-    const productionData = data?.map((property, index) => ({
-        id: index + 1,
-        teamLeader: property?.team_leader ? property.team_leader : '--:--',
-        operator: property?.operator ? property?.operator : '--:--',
-        image: property?.image,
-        targetTime: property?.target_time ? property?.target_time : '--:--',
-        cycleTime: property?.act_time_cyc ? property?.act_time_cyc?.toFixed(2) : 0,
-        cycleCounts: property?.cycle_counts ? property?.cycle_counts : '--:--',
-        masterBatch: property?.master_batch ? property?.master_batch : '--:--',
-        productCode: property?.product_code ? property?.product_code : '--:--',
-        productName: property?.product_name ? property?.product_name : '--:--',
-        powerUsage: property?.consumption_kwh ? property?.consumption_kwh : '--:--',
-        countPerShot: property?.count_per_shot ? property?.count_per_shot : '--:--',
-        machineNumber: property?.machine_number ? property?.machine_number : '--:--',
-        machineStatus: property?.machine_status,
-        efficiency: property?.productivity_percent ? property?.productivity_percent : 0,
-
-        //UI
-        isActive: Number(property?.act_time_cyc) === 0 ? 'In-Active' : 'Active',
-    }))
-
     return (
         <div className='w-full h-full overflow-y-scroll flex flex-col justify-start gap-2 pr-1'>
             {products?.map(({ image, uid, product_name }, index) =>
                 <div className={`ease-in-out duration-500 bg-white rounded w-full flex flex-col justify-start gap-2 border-2 ${isExpanded === Number(uid) && 'border-green'}`} key={index}>
-                    <div className="w-full bg-white p-2 rounded lg:cursor-pointer flex items-center justify-between relative divide-x divide-greyMid" onClick={() => toggleAccordion(Number(uid))}>
+                    <div className="w-full bg-white p-2 rounded lg:cursor-pointer flex items-center justify-between relative divide-x divide-greyMid" >
                         <div className='flex items-center justify-center gap-1 w-[25%]'>
-                            <p className={`font-medium w-32 p-2 text-center rounded text-gray-500 text-sm`}>{index + 1}</p>
+                            <p className={`font-medium w-32 p-2 text-center rounded ${isExpanded === uid ? 'text-4xl text-green' : 'text-sm text-gray-500'}`}>{index + 1}</p>
                         </div>
                         <div className='flex items-center justify-center gap-1 w-[25%]'>
                             {image ?
@@ -103,8 +76,17 @@ export const ViewProductsModule = () => {
                         <div className='flex items-center justify-center gap-1 w-[25%]'>
                             <p className='text-gray-500 text-sm font-medium'>{product_name}</p>
                         </div>
-                        <div className='flex items-center justify-center gap-1 w-[25%]'>
-                            <ArrowDown size={20} strokeWidth={2} absoluteStrokeWidth color={isExpanded === Number(uid) ? colors[0]?.red : colors[0]?.green} className={`${isExpanded === Number(uid) && 'rotate-180'} lg:ease-in-out lg:duration-300`} />
+                        <div className='flex items-center justify-center gap-6 w-[25%]'>
+                            <Link href={`/products/${uid}`}>
+                                <Pencil size={30} strokeWidth={2} absoluteStrokeWidth color={colors[0]?.purple} className={`lg:ease-in-out lg:duration-300`} />
+                            </Link>
+                            {
+                                isExpanded === uid ?
+                                    <EyeOff size={30} strokeWidth={2} absoluteStrokeWidth color={colors[0]?.red} className={`lg:ease-in-out lg:duration-300`} onClick={() => toggleAccordion(Number(uid))} />
+                                    :
+                                    <Eye size={30} strokeWidth={2} absoluteStrokeWidth color={colors[0]?.green} className={`lg:ease-in-out lg:duration-300`} onClick={() => toggleAccordion(Number(uid))} />
+
+                            }
                         </div>
                     </div>
                     <hr className='w-11/12 mx-auto' />
