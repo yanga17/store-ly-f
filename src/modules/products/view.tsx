f4402552e4eadda0ef73bf9d98c76df8e97a86a8
BranbdonOnlin3 <Brandonnkawu01@gmail.com>
2024-02-02 09:11:21 +0200

    adds products details

--- src/modules/products/view.tsx
+++ src/modules/products/view.tsx
@@ -34,12 +34,14 @@ export const ViewProductsModule = () => {
         )
     }
 
+    console.log(products)
+
     return (
         <div className='w-full h-screen flex flex-col justify-start gap-3 overflow-y-scroll pb-16'>
             <TableHeaderModule headers={wmmHeaders[0]?.headers} />
             <div className='w-full h-full pb-30 overflow-y-scroll flex flex-col justify-start gap-2 pr-1 pb-30'>
-                {products?.map(({ image, uid, product_name }, index) =>
-                    <div className={`ease-in-out duration-500 bg-white rounded w-full flex flex-col justify-start gap-2 border-2 ${isExpanded === Number(uid) && 'border-green'}`} key={index}>
+                {products?.map(({ image, uid, product_name, amount_per_pallet, charging_time, cool_time, count_per_shot, enabled, master_batch, product_code, target_time, test_run_machine, total_weight, volume, weight }, index) =>
+                    <div className={`ease-in-out duration-500 bg-white rounded w-full flex flex-col justify-start gap-2 border-2 ${isExpanded === Number(uid) && 'border-green'}`} key={uid}>
                         <div className="w-full bg-white p-2 rounded lg:cursor-pointer flex items-center justify-between relative divide-x divide-greyMid" >
                             <div className='flex items-center justify-center gap-1 w-[25%]'>
                                 <p className={`font-medium w-32 p-2 text-center rounded ${isExpanded === uid ? 'text-4xl text-green' : 'text-sm text-gray-500'}`}>{index + 1}</p>
@@ -81,48 +83,48 @@ export const ViewProductsModule = () => {
                             <div className='w-full p-2 flex items-start justify-start flex-wrap gap-y-4'>
                                 <div className='w-1/4'>
                                     <p className='text-gray-500 font-medium text-sm '>Product Code:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-black font-medium'>{product_code}</p>
                                 </div>
                                 <div className='w-1/4'>
                                     <p className='text-gray-500 font-medium text-sm '>Test Machine:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-black font-medium'>{test_run_machine}</p>
                                 </div>
                                 <div className='w-1/4'>
-                                    <p className='text-gray-500 font-medium text-sm '>Weight:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-gray-500 font-medium text-sm '>Weight <span className='text-gray-500 text-xs'>(g)</span>:</p>
+                                    <p className='text-black font-medium'>{weight}</p>
                                 </div>
                                 <div className='w-1/4'>
-                                    <p className='text-gray-500 font-medium text-sm '>Cool Time:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-gray-500 font-medium text-sm '>Cool Time <span className='text-gray-500 text-xs'>(sec)</span>:</p>
+                                    <p className='text-black font-medium'>{cool_time}</p>
                                 </div>
                                 <div className='w-1/4'>
-                                    <p className='text-gray-500 font-medium text-sm '>Charging Time:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-gray-500 font-medium text-sm '>Charging Time <span className='text-gray-500 text-xs'>(sec)</span>:</p>
+                                    <p className='text-black font-medium'>{charging_time}</p>
                                 </div>
                                 <div className='w-1/4'>
-                                    <p className='text-gray-500 font-medium text-sm '>Target Time:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-gray-500 font-medium text-sm '>Target Time <span className='text-gray-500 text-xs'>(sec)</span>:</p>
+                                    <p className='text-black font-medium'>{target_time}</p>
                                 </div>
                                 <div className='w-1/4'>
                                     <p className='text-gray-500 font-medium text-sm '>Cavity:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-black font-medium'>{count_per_shot}</p>
                                 </div>
                                 <div className='w-1/4'>
                                     <p className='text-gray-500 font-medium text-sm '>Quantity Per Palette:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-black font-medium'>{amount_per_pallet}</p>
                                 </div>
 
                                 <div className='w-1/4'>
-                                    <p className='text-gray-500 font-medium text-sm '>Total Weight:</p>
+                                    <p className='text-gray-500 font-medium text-sm '>Total Weight <span className='text-gray-500 text-xs'>(g)</span>:</p>
                                     <p className='text-black font-medium'>--:--</p>
                                 </div>
                                 <div className='w-1/4'>
-                                    <p className='text-gray-500 font-medium text-sm '>Master Batch:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-gray-500 font-medium text-sm '>Master Batch <span className='text-gray-500 text-xs'>(%)</span>:</p>
+                                    <p className='text-black font-medium'>{master_batch}</p>
                                 </div>
                                 <div className='w-1/4'>
-                                    <p className='text-gray-500 font-medium text-sm '>Volume:</p>
-                                    <p className='text-black font-medium'>--:--</p>
+                                    <p className='text-gray-500 font-medium text-sm '>Volume <span className='text-gray-500 text-xs'>(L)</span>:</p>
+                                    <p className='text-black font-medium'>{volume}</p>
                                 </div>
                             </div>
                         }
